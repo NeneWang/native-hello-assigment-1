964ce8f88ecec2b3bddff7d9431e5c455793492f
NelsonWang <wangnelson2@gmail.com>
2023-11-15 20:13:22 -0500

    aoo | dots

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [dotsShow, setDotsShow] = useState(3);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDotsShow((dotsShow) => dotsShow === 3 ? 0 : dotsShow + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  })
+
+
+  // Create an array of dots based on the value of dotsShow
+  const dotsArray = Array.from({ length: dotsShow }, (_, index) => index);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text} >Hola, Coder!</Text>
       <StatusBar style="auto" />
+      {/* Space */}
+      <Text> </Text>
+      <Text>de Nelson</Text>
+      <View style={styles.loading}>
+        <Text style={styles.dots}> </Text>
+        {dotsArray.map((dot) => (
+          <Text key={dot} style={styles.dots}>
+            .
+          </Text>
+        ))}
+        <Text> </Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +44,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  loading: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    flexDirection: 'row',
+  },
+  dots:{
+    fontSize: 50,
+    fontWeight: 'bold',
+    flexDirection: 'row',
+  
+  }
 });
